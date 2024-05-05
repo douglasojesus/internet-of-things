9a80bdb19a1508604d723730c1513ea15b6932f9
Douglas Jesus <dougecomp@gmail.com>
2024-05-05 15:03:29 -0300

    update readme

--- README.md
+++ README.md
@@ -1,15 +1,49 @@
 # internet-of-things
 
+<div align="center">
+
+# Internet of things (IOT)
+
+</div>
+
+# Resumo
+
+# Arquitetura da solução (componentes e mensagens)
+
+# Protocolo de comunicação entre dispositivo e Broker 
+
+## Camada de aplicação
+
+## Camada de transporte
+
+# Interface da Aplicação (REST)
+
+# Formatação, envio e tratamento de dados
+
+# Tratamento de conexões simultâneas (threads)
+
+# Gerenciamento do dispositivo
+
+# Desempenho (uso de cache no Broker, filas, threads, etc.)
+
+# Confiabilidade da solução (tratamento das conexões)
+
+# Documentação do código
+
+# Emprego do Docker
+
 Siga os seguintes passos no terminal para executar o broker: 
 - git clone https://github.com/douglasojesus/internet-of-things
 - cd internet-of-things/broker/
 - sudo docker build -t broker .
-- sudo docker run --network='host' -it --name container server
+- sudo docker run --network='host' -it --name container_broker broker
 
 Com isso, o servidor Broker já está rodando na porta 1026.
 
 Para emular o dispositivo, abra outro terminal e navegue no diretório clonado:
-- python3 internet-of-things/sensor/dispositivo.py
+- cd internet-of-things/sensor/
+- sudo docker build -t sensor .
+- sudo docker run --network='host' -it --name container_sensor sensor
 
 Para exibir a aplicação, abra outro terminal e navegue no diretório clonado:
 - python3 internet-of-things/aplicacao/main.py
--- sensor/dispositivo.py
+++ sensor/dispositivo.py
@@ -1,9 +1,6 @@
-"""
-PARA RECEBER O VALOR SERÁ ATRAVÉS DO TCP:
-"""
-
 import socket
 import random
+import threading
 import time
 
 HOST = '0.0.0.0'
@@ -11,14 +8,12 @@ UDP_PORT = 1025  # Porta para comunicação UDP - porta do broker
 SENSOR_ID = 'sensor1'  # Identificador do sensor
 MEU_IP = socket.gethostbyname(socket.gethostname())
 
-def recebe_conexao(server): # ELE TEM QUE SEMPRE RECEBER -> PRECISA INICIAR O RECEBE CONEXÃO ANTES DO BROKER.
+def recebe_conexao(server): 
     conexao, client_addr = server.accept()
     data = conexao.recv(1024).decode()
-    print(f"Dispositivo conectado: {client_addr}") # broker
+    print(f"Dispositivo conectado: {client_addr}") 
     print(f'Dados recebidos: {data}')
-    # lidar com os comandos. retornar o id_aplicacao junto com o dados em solicita conexao 
     conexao.close()
-
     return client_addr, data
 
 def solicita_conexao(data, sock, SERVER_IP):
@@ -26,29 +21,16 @@ def solicita_conexao(data, sock, SERVER_IP):
     sock.sendto(data.encode(), (SERVER_IP, UDP_PORT))
     print(f"Dados enviados: {data}")
 
-def envia_porta_para_broker(server, SERVER_IP, TCP_PORT, NOME, MEDICAO):
+def envia_porta_para_broker(SERVER_IP, TCP_PORT, NOME, MEDICAO):
     time.sleep(1)
-    data = f"('{NOME}', '{MEDICAO}', {TCP_PORT}, '{MEU_IP}')" # format: (nome, medicao, porta, ip)  
+    data = f"('{NOME}', '{MEDICAO}', {TCP_PORT}, '{MEU_IP}')" 
     sock.sendto(data.encode(), (SERVER_IP, UDP_PORT))
     print(SERVER_IP, UDP_PORT, "enviado para broker")
 
 def generate_number():
-    return round(random.uniform(20, 30), 2)  # Simula temperatura entre 20°C e 30°C
+    return round(random.uniform(20, 30), 2)  
 
-if __name__ == '__main__':
-    SERVER_IP = input("Qual o IP do servidor? >> ")
-    TCP_PORT = int(input("Qual a porta desse dispositivo (01028-65535)? >> "))
-    while TCP_PORT < 1028 or TCP_PORT > 65535 or TCP_PORT == 8000:
-        TCP_PORT = int(input("A porta precisa estar entre 01028 e 65535. >> "))
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, TCP_PORT))
-    server.listen(1) #Só escuta de um broker
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    entrada = input("Dispositivo já foi instalado no broker? (S/N)\n>> ")
-    if entrada.lower() == "n":
-        NOME = input("Qual o nome deste dispositivo? >> ")
-        MEDICAO = input("Que tipo de medição este dispositivo faz? >> ")
-        envia_porta_para_broker(server, SERVER_IP, TCP_PORT, NOME, MEDICAO)
+def listen_to_socket(server):
     while True:
         print("Dispositivo Startado")
         broker_info = recebe_conexao(server)
@@ -56,9 +38,42 @@ if __name__ == '__main__':
         value = generate_number()
         solicita_conexao(value, sock, SERVER_IP)
         time.sleep(1)
-    conexao.close()
-    server.close()
-    sock.close()
 
-# FLUXO:
-# ATIVA SERVIDOR TCP -> RECEBE SOLICITAÇÃO DO BROKER -> SOLICITA CONEXÃO -> RESPONDE A TEMPERATURA PARA O BROKER 
+def habilita_desabilita_conexao():
+    anterior = ''
+    while True:
+        time.sleep(1)
+        comando = input("Digite 'conectar' para escutar na porta ou 'desconectar' para parar de escutar: ")
+        if comando != anterior:
+            anterior = comando
+            if comando.lower() == "conectar":
+                listener_thread = threading.Thread(target=listen_to_socket, args=(server,))
+                listener_thread.start()
+            elif comando.lower() == "desconectar":
+                listener_thread.join()
+                print("Desconectado. Para o broker se conectar novamente, digite conectar.")
+            else:
+                print("Comando inválido.")
+
+
+if __name__ == '__main__':
+    SERVER_IP = input("Qual o IP do servidor? >> ")
+    TCP_PORT = int(input("Qual a porta desse dispositivo (01028-65535)? >> "))
+    while TCP_PORT < 1028 or TCP_PORT > 65535 or TCP_PORT == 8000:
+        TCP_PORT = int(input("A porta precisa estar entre 01028 e 65535. >> "))
+    
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, TCP_PORT))
+    server.listen(1) 
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    entrada = input("Dispositivo já foi instalado no broker? (S/N)\n>> ")
+    if entrada.lower() == "n":
+        NOME = input("Qual o nome deste dispositivo? >> ")
+        MEDICAO = input("Que tipo de medição este dispositivo faz? >> ")
+        envia_porta_para_broker(SERVER_IP, TCP_PORT, NOME, MEDICAO)
+    
+    thread_habilita = threading.Thread(target=habilita_desabilita_conexao)
+    thread_habilita.start()
+    
